dbb35c9569cf2e3f190d88534f1c8babf7c64934
obi kelvin <ebukakels@gmail.com>
2025-10-19 21:11:08 +0100

    dashboard

--- nasfa_payroll/src/App.jsx
+++ nasfa_payroll/src/App.jsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Auth/Login.jsx";
-import Home from "./components/Home/Home.jsx";
+import Dashboard from "./components/Home/dashboard.jsx";
 
 export default function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboardda />} />
       </Routes>
     </>
   );
